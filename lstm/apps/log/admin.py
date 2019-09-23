32cf49470f8ccfdfed553267d65e7fa4a481d00d
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-09-23 13:31:12 -0500

    Added project

---
+++ lstm/apps/log/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from apps.log.models import Log
+
+admin.site.register(Log)
---
+++ lstm/apps/log/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Log(models.Model):
+    name = models.CharField(max_length=50)
+    number_of_traces = models.IntegerField()
+    number_of_events = models.IntegerField()
+    number_of_activities = models.IntegerField()
