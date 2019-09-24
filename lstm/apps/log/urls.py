a34ac0133242f693bb3ffb709f169df328b1d991
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-09-23 22:10:24 -0500

    Connected index with step 1

--- lstm/apps/log/urls.py
+++ lstm/apps/log/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path  
-from .views import index
+from .views import index, step1
 
 urlpatterns = [
     path('', index, name='log'),
+    path('step1/', step1, name='step1')
 ]
--- lstm/apps/log/views.py
+++ lstm/apps/log/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'log/index.html')
+
+def step1(request):
+    return render(request, 'log/step1.html')
