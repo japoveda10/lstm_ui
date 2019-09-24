af59e819ba1aac050476d06270d5b1cfa271e790
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-09-24 13:34:35 -0500

    Added logs to select log form

--- lstm/apps/log/forms.py
+++ lstm/apps/log/forms.py
@@ -3,8 +3,22 @@ from django import forms
 class SelectLogForm(forms.Form):
     LOG_1 = 'log_1'
     LOG_2 = 'log_2'
+    LOG_3 = 'log_3'
+    LOG_4 = 'log_4'
+    LOG_5 = 'log_5'
+    LOG_6 = 'log_6'
+    LOG_7 = 'log_7'
+    LOG_8 = 'log_8'
+    LOG_9 = 'log_9'
     LOG_CHOICES = (
-        (LOG_1, u"Log 1"),
-        (LOG_2, u"Log 2")
+        (LOG_1, u"Helpdesk"),
+        (LOG_2, u"BPI 2012 complete"),
+        (LOG_3, u"BPI 2012 W sub-process"),
+        (LOG_4, u"BPI 2013 complete"),
+        (LOG_5, u"BPI 2015-1"),
+        (LOG_6, u"BPI 2015-2"),
+        (LOG_7, u"BPI 2015-3"),
+        (LOG_8, u"BPI 2015-4"),
+        (LOG_9, u"BPI 2015-5"),
     )
     logs = forms.ChoiceField(choices=LOG_CHOICES)
